f8164b5e02a72e7058a47ccfb3301c86e535db5c
MovingOtarubombG <kyouku918@gmail.com>
2020-02-14 16:33:47 +0900

    弾が壁に当たったらエフェクトが発生

---
+++ GameJamShooting/Assets/matui/effect_matui.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class effect_matui : MonoBehaviour
+{
+
+    float Delete_time;
+    float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time = Time.deltaTime;
+        Delete_time += time;
+
+        if (Delete_time > 0.5f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
