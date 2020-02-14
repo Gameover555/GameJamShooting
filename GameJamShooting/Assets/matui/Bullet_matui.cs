fbe976cbb44b1906155d29e9555e0807ed68ca63
MovingOtarubombG <kyouku918@gmail.com>
2020-02-14 14:35:05 +0900

    プレイヤーが移動(仮) グラフィックのお試し

---
+++ GameJamShooting/Assets/matui/Bullet_matui.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_matui : MonoBehaviour
+{
+    float Delete_time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //
+        Delete_time += Time.deltaTime;
+        transform.Translate(0, 0.2f, 0);
+        if (Delete_time > 3.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ GameJamShooting/Assets/matui/Player_matui.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_matui : MonoBehaviour
+{
+
+    public float speed;//速さ
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 移動処理
+        transform.Translate
+            (Input.GetAxis("Horizontal") * 0.1f * speed,
+            Input.GetAxis("Vertical") * 0.1f * speed,
+            0);
+
+        //キー、ステックの取得
+        float Ver_1 = Input.GetAxis("Vertical");
+        float Hor_1 = Input.GetAxisRaw("Horizontal");
+
+
+
+    }
+}
