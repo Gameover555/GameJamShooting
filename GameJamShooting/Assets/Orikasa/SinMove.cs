c62f872258f9d74bfd133325a336d8905e01ccd0
OrikasaEuro <0wk74m053261p7j@ezweb.ne.jp>
2020-02-15 11:31:13 +0900

    [Add]タイトルロゴの実装及び移動の処理

---
+++ GameJamShooting/Assets/Orikasa/SinMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float sin = Mathf.Sin(Time.time) * 0.6f;
+        this.transform.position = new Vector3(0, 1.5f + sin, 0);
+    }
+}
